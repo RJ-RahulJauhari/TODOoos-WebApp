94df72642d69f6dafed8a19f245c628efff96945
Rahul Jauhari <rj.rahul.jauhari@gmail.com>
2023-11-13 01:43:19 +0530

    finallll

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import './Navbar.scss'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <div className='logo clickable flex1'>
+      <div className='logo clickable flex4'>
         <h1>TODOoos...</h1>
       </div>
-      {/* <div className='navigation-tabs flex3'>
-        <Link className="tab hoverable scaleUp" to="/"><p>Home</p></Link>
-        <Link className="tab hoverable scaleUp" to="/analytics"><p>Analytics</p></Link>
-      </div> */}
+      <div className='navigation-tabs flex1'>
+        {/* <Link className="tab hoverable scaleUp" to="/"><p>Home</p></Link>
+        <Link className="tab hoverable scaleUp" to="/analytics"><p>Analytics</p></Link> */}
+        <div className='beta'>Beta</div>
+      </div>
     </div>
   )
 }
